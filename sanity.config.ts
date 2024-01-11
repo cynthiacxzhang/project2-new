a24fb85abd5b0866da294588ae83ec6d9431a79f
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2024-01-11 11:26:09 -0500

    sanity setup

---
+++ sanity.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "sanity";
+
+const config = defineConfig({
+    projectId: "zn60c1zt",
+    dataset: "production",
+    tite: "My Personal Website",
+    apiVersion: "2024-01-11"
+
+})
+
+export default config;
